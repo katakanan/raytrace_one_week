40071c5585378855bdd30bd3083234cb366d7538
katakanan <>
2020-08-18 17:01:30 +0900

    output an image

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate image;
+
+use image::{Rgba, RgbaImage};
+
+pub fn render(w: u32, h: u32) -> RgbaImage {
+    let mut img = RgbaImage::new(w, h);
+    for y in 0..h {
+        for x in 0..w {
+            let ny = h - y - 1;
+            let nx = x;
+
+            let r = (nx as f64) / (w as f64);
+            let g = (ny as f64) / (h as f64);
+            let b = 0.2;
+
+            let ir = (255.99 * r) as u8;
+            let ig = (255.99 * g) as u8;
+            let ib = (255.99 * b) as u8;
+
+            img.put_pixel(x, y, Rgba([ir, ig, ib, 255]))
+        }
     }
+    img
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+extern crate image;
 extern crate renderer;
+
+use image::ImageFormat;
 
 fn main() {
     println!("Hello World");
+    let w = 200;
+    let h = 100;
+
+    let _ = renderer::render(w, h)
+        .save_with_format(std::path::Path::new("test.jpg"), ImageFormat::Jpeg)
+        .unwrap();
 }
